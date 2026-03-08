2ab3a358e773677362145e5ca8af88e4c8ba115b
SrjAdhikari <surajadhikari055@gmail.com>
2026-03-08 19:13:38 +0900

    feat: add File model with schema validation

---
+++ src/models/file.model.js
@@ -0,0 +1,38 @@
+//* src/models/file.model.js
+
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+const fileSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
+			minlength: 3,
+		},
+		extension: {
+			type: String,
+			required: true,
+			trim: true,
+			lowercase: true,
+		},
+		parentDirId: {
+			type: Schema.Types.ObjectId,
+			ref: "Directory",
+			required: true,
+		},
+		userId: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+	},
+	{
+		strict: "throw",
+		timestamps: true,
+	},
+);
+
+const File = model("File", fileSchema);
+export default File;
