4f6968b9fd7007a82faed8a553ed9cebf44a411d
SrjAdhikari <surajadhikari055@gmail.com>
2026-03-08 19:13:12 +0900

    feat: add Directory model with schema validation

---
+++ src/models/directory.model.js
@@ -0,0 +1,32 @@
+//* src/models/directory.model.js
+
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+const directorySchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
+			minlength: 3,
+		},
+		parentDirId: {
+			type: Schema.Types.ObjectId,
+			ref: "Directory",
+			default: null,
+		},
+		userId: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+	},
+	{
+		strict: "throw",
+		timestamps: true,
+	},
+);
+
+const Directory = model("Directory", directorySchema);
+export default Directory;
