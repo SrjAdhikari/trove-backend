913f3b3faa8b45e7833a2315b202e2f67f4954b1
SrjAdhikari <surajadhikari055@gmail.com>
2026-03-09 21:21:46 +0900

    feat: add Express app with core middleware, health check, and error handling

---
+++ src/app.js
@@ -0,0 +1,82 @@
+//* src/app.js
+
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import envConfig from "./constants/env.js";
+import httpStatus from "./constants/httpStatus.js";
+import appErrorCode from "./constants/appErrorCode.js";
+
+import AppError from "./errors/AppError.js";
+import globalErrorHandler from "./middlewares/error.middleware.js";
+
+const { APP_ORIGIN, NODE_ENV } = envConfig;
+const { OK, NOT_FOUND } = httpStatus;
+const { ROUTE_NOT_FOUND } = appErrorCode;
+
+const app = express();
+const allowedOrigins = [APP_ORIGIN];
+
+//* ==============================
+//* EXPRESS CORE MIDDLEWARE
+//* ==============================
+app.use(
+	cors({
+		origin: allowedOrigins,
+		credentials: true,
+		methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+	}),
+);
+
+app.use(express.json({ limit: "1mb" }));
+app.use(cookieParser());
+
+//* ==============================
+//* REQUEST LOGGER
+//* ==============================
+app.use((req, res, next) => {
+	if (NODE_ENV === "development") {
+		console.log(
+			`Request Method: ${req.method}`,
+			`URL: ${req.url}`,
+			`Headers: ${JSON.stringify(req.headers)}`,
+			`Body: ${JSON.stringify(req.body)}`,
+		);
+	}
+	next();
+});
+
+//* ==============================
+//* HEALTH CHECK & HOME ROUTE
+//* ==============================
+app.get("/health", (req, res) => {
+	res.status(OK).json({
+		success: true,
+		message: "Healthy 👍",
+	});
+});
+
+app.get("/", (req, res) => {
+	res.status(OK).json({
+		success: true,
+		message: "Welcome to the Trove API",
+	});
+});
+
+//* ==============================
+//* 404 + GLOBAL ERROR HANDLER
+//* ==============================
+app.use("/{*splat}", (req, res, next) => {
+	next(
+		new AppError(
+			`Route ${req.method} ${req.originalUrl} not found`,
+			NOT_FOUND,
+			ROUTE_NOT_FOUND,
+		),
+	);
+});
+
+app.use(globalErrorHandler);
+
+export default app;
