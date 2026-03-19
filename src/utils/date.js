905d3c85ee88c14415c765e35a7edd55f2aa89ba
SrjAdhikari <surajadhikari055@gmail.com>
2026-03-19 20:46:40 +0900

    refactor(utils): extract dynamic time durations into reusable constants

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,5 +1,14 @@
 //* src/utils/date.js
 
-const sevenDaysFromNow = () => new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+export const ONE_MINUTE_MS = 60 * 1000;
+export const TEN_MINUTES_MS = 10 * 60 * 1000;
+export const ONE_HOUR_MS = 60 * 60 * 1000;
+export const SEVEN_DAYS_MS = 7 * 24 * 60 * 60 * 1000;
 
-export { sevenDaysFromNow };
+const tenMinutesFromNow = () => new Date(Date.now() + TEN_MINUTES_MS);
+
+const oneHourFromNow = () => new Date(Date.now() + ONE_HOUR_MS);
+
+const sevenDaysFromNow = () => new Date(Date.now() + SEVEN_DAYS_MS);
+
+export { tenMinutesFromNow, oneHourFromNow, sevenDaysFromNow };
