176be7098d625fb26785d1264248374221329c3d
SrjAdhikari <surajadhikari055@gmail.com>
2026-03-22 12:56:24 +0900

    feat(utils): add signed auth cookie helpers for session management

---
+++ src/utils/cookies.js
@@ -0,0 +1,22 @@
+//* src/utils/cookies.js
+
+import { SEVEN_DAYS_MS } from "./date.js";
+import envConfig from "../constants/env.js";
+
+const { NODE_ENV } = envConfig;
+
+const setAuthCookie = (res, sessionId) => {
+	res.cookie("token", sessionId, {
+		httpOnly: true,
+		signed: true,
+		sameSite: "lax",
+		maxAge: SEVEN_DAYS_MS,
+		secure: NODE_ENV === "production",
+	});
+};
+
+const clearAuthCookie = (res) => {
+	res.clearCookie("token");
+};
+
+export { setAuthCookie, clearAuthCookie };
