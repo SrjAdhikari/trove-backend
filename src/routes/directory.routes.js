f4b8072466e30a3eb84eef2f44a45355bf5ac2cc
SrjAdhikari <surajadhikari055@gmail.com>
2026-03-26 19:54:36 +0900

    feat(routes): add auth and ObjectId validation to directory routes

--- src/routes/directory.routes.js
+++ src/routes/directory.routes.js
@@ -7,36 +7,46 @@
 
 import { Router } from "express";
 import {
-	createDirectory,
-	deleteDirectory,
-	getDirectoryById,
-	updateDirectory,
+	getDirectoryHandler,
+	createDirectoryHandler,
+	updateDirectoryHandler,
+	deleteDirectoryHandler,
 } from "../controllers/directory.controller.js";
+import authenticate from "../middlewares/auth.middleware.js";
+import validateId from "../middlewares/validateId.middleware.js";
 
 const directoryRouter = Router();
+
+// All directory routes require authentication
+directoryRouter.use(authenticate);
+
+// Validate ID parameters
+["id", "parentDirId"].forEach((param) => {
+	directoryRouter.param(param, validateId);
+});
 
 /**
  * Get directory contents by id
  * @route GET /api/directories/{:id}
  */
-directoryRouter.get("{/:id}", getDirectoryById);
+directoryRouter.get("{/:id}", getDirectoryHandler);
 
 /**
  * Create a new directory
  * @route POST /api/directories/{:parentDirId}
  */
-directoryRouter.post("{/:parentDirId}", createDirectory);
+directoryRouter.post("{/:parentDirId}", createDirectoryHandler);
 
 /**
  * Update (Rename) a directory
  * @route PATCH /api/directories/{:id}
  */
-directoryRouter.patch("/:id", updateDirectory);
+directoryRouter.patch("/:id", updateDirectoryHandler);
 
 /**
  * Delete a directory and all its children
  * @route DELETE /api/directories/{:id}
  */
-directoryRouter.delete("/:id", deleteDirectory);
+directoryRouter.delete("/:id", deleteDirectoryHandler);
 
 export default directoryRouter;
